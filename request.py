54a773edb4ef7cf9619a23ee96a61881f2ef0676
Habibur Rahaman <habiburg3@gmail.com>
2025-10-29 14:41:35 +0600

    Genderize API

--- request.py
+++ request.py
@@ -1,27 +1,13 @@
 import requests
 import json
 
-# List of all objects (Get)
-# URL = "https://api.restful-api.dev/objects/1"
-# List of objects by ids (Get)
-# URL = "https://api.restful-api.dev/objects?id=3&id=5&id=10"
-# Single object (Get)
-URL = "https://api.restful-api.dev/objects/7"
-
-# Add object (Post)
-# URL = "https://api.restful-api.dev/objects"
-
-# Update object (Put)
-# URL = "https://api.restful-api.dev/objects/7"
-
-# Partially update object (Patch)
-# URL = "https://api.restful-api.dev/objects/7"
-
-# Delete object (Delete)
-# URL = "https://api.restful-api.dev/objects/6"
-
-
-r = requests.get(URL)
-r_json = r.json()
-data = json.dumps(r_json, indent=4)
-print(data)
+URL = "https://api.genderize.io"
+myName = input("Enter name: ")
+data = {
+    "name": myName
+}
+r = requests.get(url=URL, params=data)
+gender = r.json()
+mygen = gender["gender"]
+myProba = gender["probability"]
+print(f"Gender of {myName} is {mygen} with probability {myProba}")
